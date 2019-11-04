e276815081570f7da1f0ef01a84b2de40e6f3ad3
God_Z <1042813942@qq.com>
2019-11-04 15:26:35 +0800

    增加图片文字组件

---
+++ src/components/img.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="img">
+    <img :src="attribute.url" :style="styles">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'img',
+  data() {
+    return {};
+  },
+  props: {
+    attribute: null,
+    styles: null
+  },
+  methods: {
+  },
+};
+
+</script>
+
+<style lang="less"></style>
---
+++ src/components/text.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text">
+    <h2 :style="styles">{{attribute.title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'text',
+  data() {
+    return {};
+  },
+  props: {
+    attribute: null,
+    styles: null
+  },
+  methods: {
+  },
+};
+
+</script>
+
+<style lang="less"></style>
